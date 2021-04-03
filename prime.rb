471118b7351a5c9695b9200825bc43292f161733
Milan Mody <milanm1120@gmail.com>
2021-04-03 01:56:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num == 0 || num ==1 || num < 2
+    false
+  else
+    (2..num/2).none? do |x|
+      num % x == 0
+    end
+  end
+end
+
+
+puts prime?(5)
+puts prime?(6)
+puts prime?(1)
